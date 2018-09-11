dadb690b22f42e794340ec78dcfd4f3d4693bad5
Timothy R. Koscik <timkoscik@gmail.com>
2018-09-11 13:42:14 -0500

    Update init_data_structure.sh

--- tools/init_data_structure.sh
+++ tools/init_data_structure.sh
@@ -1,32 +1,5 @@
 #! /bin/bash
 
-# =============================================================================
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-# 
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-# 
-# For more information, please refer to <http://unlicense.org>
-# =============================================================================
-
 # Help ------------------------------------------------------------------------
 function Usage {
     cat <<USAGE
@@ -58,26 +31,26 @@ USAGE
 # Parse input operators -------------------------------------------------------
 while getopts "r:p:g:h" option
 do
-case "${option}"
-in
-  r) # researcher_directory
-    researcherRoot=${OPTARG}
-    ;;
-  p) # project_name
-    projectName=${OPTARG}
-    ;;
-  g) # group permissions
-    groupOwn=${OPTARG}
-    ;;
-  h) # help
-    Usage >&2
-    exit 0
-    ;;
-  *) # unknown options
-    echo "ERROR: Unrecognized option -$OPT $OPTARG"
-    exit 1
-    ;;
-esac
+  case "${option}"
+  in
+    r) # researcher_directory
+      researcherRoot=${OPTARG}
+      ;;
+    p) # project_name
+      projectName=${OPTARG}
+      ;;
+    g) # group permissions
+      groupOwn=${OPTARG}
+      ;;
+    h) # help
+      Usage >&2
+      exit 0
+      ;;
+    *) # unknown options
+      echo "ERROR: Unrecognized option -$OPT $OPTARG"
+      exit 1
+      ;;
+  esac
 done
 
 # Create Directory tree -------------------------------------------------------
